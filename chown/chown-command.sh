c69a11b8c1e976fa751dbfa4801c5dcddea5ef03
Mati <matiboux@gmail.com>
2025-02-20 16:00:48 +0100

    Update env vars & Support to set group id (#2)

--- chown/chown-command.sh
+++ chown/chown-command.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 set -e
 
-if [ -z "$VOLUME_PATHS" ]; then
+if [ -z "$VOLUMES" ]; then
 	exit 0
 fi
 
 while IFS= read -r VOLUME_PATH; do
 	if [ -n "$VOLUME_PATH" ]; then
-		echo "Chown '$VOLUME_PATH' to '${UID:-1000}:0'"
-		chown -R ${UID:-1000}:0 ${VOLUME_PATH}
+		echo "Chown '$VOLUME_PATH' to '${UID:-1000}:${GID:-0}'"
+		chown -R "${UID:-1000}:${GID:-0}" "${VOLUME_PATH}"
 	fi
 done <<EOF
-$VOLUME_PATHS
+$VOLUMES
 EOF
 
 exit 0
